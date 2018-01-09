2d649b33497ef51fa376adb84974b386f8e2928d
Prakash Rao Vaddina - Windows <prakash.vaddina@gmail.com>
2018-01-09 22:12:44 +0100

    A simple example on usage of a Future

---
+++ multithreading/future_simple/future.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <future>
+#include <chrono>
+
+int sleepyhead()
+{
+    auto num = 0;
+    while( num < 10 )
+    {
+        std::cout << num << std::endl;
+        std::chrono::milliseconds m(1000);
+        std::this_thread::sleep_for(m);
+        ++num;
+    }
+
+    return num;
+}
+
+int main()
+{
+    auto f = std::async(std::launch::async, sleepyhead);
+    std::cout << "Value returned after the async task has finished = " << f.get() << std::endl;
+
+    f = std::async(std::launch::deferred, sleepyhead);
+    std::cout << "Value returned after the deferred task has finished = " << f.get() << std::endl;
+
+    return 0;
+}
+
+
+
+
