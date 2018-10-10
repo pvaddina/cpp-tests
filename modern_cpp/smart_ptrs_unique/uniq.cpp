c5e4c15f8d204674b8d18846bd9188ae88466edc
Prakash Rao Vaddina <prakash.vaddina@gmail.com>
2018-10-10 21:36:29 +0200

    Update the example with smart pointers

--- modern_cpp/smart_ptrs_unique/uniq.cpp
+++ modern_cpp/smart_ptrs_unique/uniq.cpp
@@ -5,9 +5,23 @@
 #include <memory>
 #include <array>
 
+struct CopyableFooStruct
+{
+  CopyableFooStruct() = default;
+  CopyableFooStruct(const CopyableFooStruct&) = delete;
+  //CopyableFooStruct& operator=(const CopyableFooStruct&) = delete;
+  CopyableFooStruct(std::string&& _s) : s(_s) {}
+  CopyableFooStruct(int i, std::string&& _s) : p(i), s(_s) {}
+  int p { 437 };
+  std::string s { "Some test string" };
+  void Print() { std::cout << "p = " << p << ", s=" << s << std::endl; }
+};
+
 struct FooStruct
 {
   FooStruct() = default;
+  FooStruct(const FooStruct&) = delete;
+  FooStruct& operator=(const FooStruct&) = delete;
   FooStruct(std::string&& _s) : s(_s) {}
   FooStruct(int i, std::string&& _s) : p(i), s(_s) {}
   int p { 437 };
@@ -23,41 +37,41 @@ namespace T1
     std::cout << "In T1::Test" << std::endl;
 
     // Can be used for logical operations
-    std::unique_ptr<FooStruct> p1;
+    std::unique_ptr<CopyableFooStruct> p1;
     if (!p1) { std::cout << "p1 is not initialized" << std::endl; }
 
-    std::unique_ptr<FooStruct> p2 { new FooStruct };
+    std::unique_ptr<CopyableFooStruct> p2 { new CopyableFooStruct };
     if (p2) { std::cout << "p2 is initialized directly" << std::endl; }
 
     // make_shared usage
-    std::unique_ptr<FooStruct> p3 = std::make_unique<FooStruct>();
+    std::unique_ptr<CopyableFooStruct> p3 = std::make_unique<CopyableFooStruct>();
     if (p3) { std::cout << "p3 is initialized using make_unique" << std::endl; }
-    std::unique_ptr<FooStruct> p4 = std::make_unique<FooStruct>("Some rvalue string");
+    std::unique_ptr<CopyableFooStruct> p4 = std::make_unique<CopyableFooStruct>("Some rvalue string");
     if (p4) { std::cout << "p4 is initialized using make_unique with arguments" << std::endl; }
     std::cout << "p4->p=" << p4->p << ", p4->s=" << p4->s << std::endl;        
 
     // Array items
-    std::unique_ptr<FooStruct[]> p5_array;
-    if(!p5_array) {  std::cout << "p5 (array of FooStruct) is not initialized" << std::endl; }
-    p5_array.reset( new FooStruct[3] );
-    p5_array[0] = FooStruct { 1, "one" };
-    p5_array[1] = FooStruct { 2, "two" };
-    p5_array[2] = FooStruct { 3, "three" };
+    std::unique_ptr<CopyableFooStruct[]> p5_array;
+    if(!p5_array) {  std::cout << "p5 (array of CopyableFooStruct) is not initialized" << std::endl; }
+    p5_array.reset( new CopyableFooStruct[3] );
+    p5_array[0] = CopyableFooStruct { 1, "one" };   // Assignment operator called. A new CopyableFooStruct is being assigned
+    p5_array[1] = CopyableFooStruct { 2, "two" };   // Assignment operator called. A new CopyableFooStruct is being assigned
+    p5_array[2] = CopyableFooStruct { 3, "three" }; // Assignment operator called. A new CopyableFooStruct is being assigned
 
-    auto p6_array = std::make_unique<FooStruct[]>(4);
-    p6_array[0] = FooStruct { 1, "one" };
-    p6_array[1] = FooStruct { 2, "two" };
-    p6_array[2] = FooStruct { 3, "three" };
-    p6_array[3] = FooStruct { 4, "four" };
+    auto p6_array = std::make_unique<CopyableFooStruct[]>(4);
+    p6_array[0] = CopyableFooStruct { 1, "one" };   // Assignment operator called. A new CopyableFooStruct is being assigned
+    p6_array[1] = CopyableFooStruct { 2, "two" };   // Assignment operator called. A new CopyableFooStruct is being assigned
+    p6_array[2] = CopyableFooStruct { 3, "three" }; // Assignment operator called. A new CopyableFooStruct is being assigned
+    p6_array[3] = CopyableFooStruct { 4, "four" };  // Assignment operator called. A new CopyableFooStruct is being assigned
   }
 }
 
 
 namespace T2
 {
-  void CusDelFunction(FooStruct* p)
+  void CusDelFunction(CopyableFooStruct* p)
   {
-    std::cout << "In the custom deleter of FooStruct" << std::endl;
+    std::cout << "In the custom deleter of CopyableFooStruct" << std::endl;
     delete p;
   }
 
@@ -66,16 +80,15 @@ namespace T2
     std::cout << "In T2::Test" << std::endl;
 
     // A function as a custom destructor
-    std::unique_ptr<FooStruct, decltype(&CusDelFunction)> p1 { new FooStruct, &CusDelFunction };
-
+    std::unique_ptr<CopyableFooStruct, decltype(&CusDelFunction)> p1 { new CopyableFooStruct, &CusDelFunction };
 
     // A Lambda as a custom destructor
-    auto CusDelLambda = [](FooStruct* p) {
-      std::cout << "In the custom deleter of FooStruct (auto)" << std::endl;
+    auto CusDelLambda = [](CopyableFooStruct* p) {
+      std::cout << "In the custom deleter of CopyableFooStruct (auto)" << std::endl;
       delete p;
     };
 
-    std::unique_ptr<FooStruct, decltype(CusDelLambda)> p2 { new FooStruct, CusDelLambda };
+    std::unique_ptr<CopyableFooStruct, decltype(CusDelLambda)> p2 { new CopyableFooStruct, CusDelLambda };
   }
 }
 
@@ -83,9 +96,9 @@ namespace T2
 namespace T3
 {
   template <typename... Ts>
-  std::unique_ptr<FooStruct> MakeFooStruct(Ts&&... args)
+  std::unique_ptr<CopyableFooStruct> MakeFooStruct(Ts&&... args)
   {
-    return std::unique_ptr<FooStruct>(new FooStruct(std::forward<Ts>(args)...));
+    return std::unique_ptr<CopyableFooStruct>(new CopyableFooStruct(std::forward<Ts>(args)...));
   }
 
   template <typename T, typename... Ts>
@@ -107,13 +120,13 @@ namespace T3
     auto p3 = MakeFooStruct(444, std::string("Two arguments"));
     p3->Print();
 
-    auto p4 = MakeAnyStruct<FooStruct>();
+    auto p4 = MakeAnyStruct<CopyableFooStruct>();
     p4->Print();
 
-    auto p5 = MakeAnyStruct<FooStruct>(std::string("Only one argument"));
+    auto p5 = MakeAnyStruct<CopyableFooStruct>(std::string("Only one argument"));
     p5->Print();
 
-    auto p6 = MakeAnyStruct<FooStruct>(444, std::string("Two arguments"));
+    auto p6 = MakeAnyStruct<CopyableFooStruct>(444, std::string("Two arguments"));
     p6->Print();
   }
 }
@@ -128,9 +141,12 @@ namespace T4
     return std::unique_ptr<T>(new T(std::forward<Ts>(args)...));
   }
 
+  template <typename... Ts>
+  using MakeFooStruct = MakeAnyStruct<CopyableFooStruct, Ts...>;
+
   void Test()
   {
-    std::cout << "In T3::Test" << std::endl;
+    std::cout << "In T4::Test" << std::endl;
 
     auto p1 = MakeFooStruct();
     p1->Print();
@@ -154,6 +170,30 @@ namespace T4
 }
 
 
+namespace T5
+{
+  template <typename T, typename... Ts>
+  std::unique_ptr<T> MakeAnyStruct(Ts&&... args)
+  {
+    return std::unique_ptr<T>(new T(std::forward<Ts>(args)...));
+  }
+
+  void Test()
+  {
+    std::cout << "In T5::Test" << std::endl;
+
+    std::vector<std::unique_ptr<FooStruct> > v;
+    v.push_back(MakeAnyStruct<FooStruct>());
+    v.push_back(MakeAnyStruct<FooStruct>("Some string"));
+    v.push_back(MakeAnyStruct<FooStruct>(46354, "Another String"));
+
+    for (auto& iter : v)
+    {
+      iter->Print();
+    }
+  }
+}
+
 
 int main()
 {
@@ -164,5 +204,8 @@ int main()
   std::cout << "\n";
 
   T3::Test();
+  std::cout << "\n";
+
+  T5::Test();
   return 0;
 }
