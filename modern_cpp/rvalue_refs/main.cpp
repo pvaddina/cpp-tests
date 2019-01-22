704c243637f262455233dceced949427a98053b4
Prakash Rao Vaddina <prakash.vaddina@gmail.com>
2019-01-22 14:31:59 +0100

    Add new test for rvalue references

--- modern_cpp/rvalue_refs/main.cpp
+++ modern_cpp/rvalue_refs/main.cpp
@@ -4,16 +4,20 @@
 #include "test4.h"
 #include "test5.h"
 #include "test6.h"
+#include "test7.h"
 
 
 int main()
 {
+#if 0
     Test1::Test1();
     Test2::Test2();
     Test3::Test3();
     Test4::Test4();
     Test5::Test5();
     Test6::Test6();
+#endif
+    Test7::Test7();
 
     return 0;
 }
---
+++ modern_cpp/rvalue_refs/test7.h
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace Test7
+{
+  struct SimpleData
+  {
+    int id = 437;
+    std::string name = "default name";
+    // Note since there is a constructor which takes rvalue references, the constructor with const references is not called for rvalues
+    SimpleData(const int& _v, const std::string& _s) : id(_v), name(_s) { std::cout << "Constructor with const references" << std::endl; }
+    SimpleData(int&& _v, std::string&& _s) : id(_v), name(_s) { std::cout << "Constructor with rvalue references" << std::endl;  }
+    // Move constructor
+    // Deliberately deleting:
+    // 1. copy constructor
+    // 2. copy assignment operator
+    // 3. move constructor
+    // 4. move assignment operator
+    SimpleData(const SimpleData&) = delete;
+    SimpleData& operator=(SimpleData&) = delete;
+    SimpleData& operator=(SimpleData&& msg) = delete;
+    SimpleData(SimpleData&& msg) = delete;
+
+    void Print() const { std::cout << "Id=" << id << ", Name=" << name << std::endl; }
+  };
+
+  void LvalueAcceptor(SimpleData& sd)
+  {
+    sd.Print();
+  }
+
+  void RvalueAcceptor(SimpleData&& sd)
+  {
+    LvalueAcceptor(sd);
+  }
+
+  void Test7()
+  {
+    RvalueAcceptor(SimpleData{437, "Roll number"});
+  }
+}
+
