3c7bccb46d8e7579f632df6acef28638ebc5062f
Prakash Rao Vaddina <prakash.vaddina@gmail.com>
2019-01-25 23:40:43 +0100

    Fix to SFINAE example to compile with GCC

--- modern_cpp/ppack_conditional_check_example/sfinae.h
+++ modern_cpp/ppack_conditional_check_example/sfinae.h
@@ -4,14 +4,11 @@
 
 namespace SFINAE
 {
-  template <typename DST, typename... Ts>
-  void SetValue(DST& dst, Ts&&... ts)
-  {
-    CheckAndSetVal(dst, std::forward<Ts>(ts)...);
-  }
-
   template <typename DST>
   void CheckAndSetVal(DST&) {}
+
+  template <typename DST, typename T1, typename T2, typename... Ts>
+  std::enable_if_t<!std::is_same_v<DST, T2> > CheckAndSetVal(DST& dst, T1&& cond, T2&& val, Ts&&... ts);
 
   template <typename DST, typename T1, typename T2, typename... Ts>
   std::enable_if_t<std::is_same_v<DST, T2> > CheckAndSetVal(DST& dst, T1&& cond, T2&& val, Ts&&... ts)
@@ -29,6 +26,12 @@ namespace SFINAE
       dst = val();
     else
       CheckAndSetVal(dst, std::forward<Ts>(ts)...);
+  }
+
+  template <typename DST, typename... Ts>
+  void SetValue(DST& dst, Ts&&... ts)
+  {
+    CheckAndSetVal(dst, std::forward<Ts>(ts)...);
   }
 
 }
