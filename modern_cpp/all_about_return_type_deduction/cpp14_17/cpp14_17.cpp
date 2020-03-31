afcc9824c66bd805e0481aa0052dc01a02e1349b
Prakash Rao Vaddina <prakash.vaddina@gmail.com>
2020-03-31 23:23:35 +0200

    New example for return type deduction

---
+++ modern_cpp/all_about_return_type_deduction/cpp14_17/cpp14_17.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
+
+namespace T1
+{
+  using FN1 = std::vector<int>(*)(const int);
+  using FN2 = std::string(*)(const int);
+
+  template<typename FN>
+  auto Process(const FN& f) // No need of any further declaration
+  {
+      return f(10);
+  }
+
+  std::vector<int> RetVec(const int i)
+  {
+      std::vector<int> retVec;
+      for (int it=0; it<i; ++it)
+          retVec.push_back(it);
+      return retVec;
+  }
+
+  std::string RetStr(const int i)
+  {
+      std::string str;
+      for (int it=0; it<i; ++it)
+          str += "* ";
+      return str;
+  }
+
+  void NoRetStr(const int i)
+  {
+      for (int it=0; it<i; ++it)
+          std::cout << "-- ";
+  }
+
+  void Test()
+  {
+      auto vec = Process(&RetVec);
+      for (auto& i: vec)
+          std::cout << i << " ";
+      std::cout << std::endl;
+          
+      auto s = Process(&RetStr);
+      std::cout << s << std::endl;
+      
+      Process(&NoRetStr);
+  }
+}
+
+int main()
+{
+  T1::Test();
+  return 0;
+}
+
+
+
