65e9d9c2632d48b33417c76edd5095015f90dc51
Prakash Rao Vaddina <prakash.vaddina@gmail.com>
2020-05-25 09:16:17 +0200

    Extend std::function example with copy/reference objects

--- modern_cpp/all_about_std_function/function.cpp
+++ modern_cpp/all_about_std_function/function.cpp
@@ -18,6 +18,23 @@ namespace T1
     }
   };
 
+  struct FooAdvanced
+  {
+    FooAdvanced(const int v) : mVal(v) { std::cout << "In FooAdvanced constructor\n"; }
+    FooAdvanced(const FooAdvanced& f) : mVal(f.mVal) { std::cout << "In FooAdvanced copy constructor\n"; }
+
+    std::string MakeStr(const int i, const std::string s)
+    {
+      std::stringstream ss;
+      ss << "Foo::MakeStrWithInternal(): i=" << i << ", s=" << s << ", mVal=" << mVal << "\n";
+      return ss.str();
+    }
+
+    private:
+      int mVal = 100;
+  };
+
+
   std::string Coo(const int i, const std::string s)
   {
     std::stringstream ss;
@@ -27,16 +44,17 @@ namespace T1
 
   void LooseFunctions()
   {
+    std::cout << "###################### In LooseFunctions()\n";
     FN f = Coo;
     std::cout << f(1, "Number-1");
 
     FN f1 {Coo};
     std::cout << f1(1, "Number-1");
-
   }
 
   void LambdaTests()
   {
+    std::cout << "\n###################### In LambdaTests()\n";
     FN fLambda {[](const int i, const std::string s)->std::string {
       std::stringstream ss;
       ss << "Test::lambda: i=" << i << ", s=" << s << "\n";
@@ -47,6 +65,7 @@ namespace T1
 
   void MemberFns()
   {
+    std::cout << "\n###################### In MemberFns()\n";
     Foo fObj;
 
     using std::placeholders::_1;
@@ -59,6 +78,35 @@ namespace T1
     };
     std::cout << f1(2, "Number-2");
   }
+
+  void DeletedObjects()
+  {
+    std::cout << "\n###################### In DeletedObjects()\n";
+    FooAdvanced* fObj { new FooAdvanced(200) };
+
+    using std::placeholders::_1;
+    using std::placeholders::_2;
+    FN f = std::bind(&FooAdvanced::MakeStr, fObj, _1, _2);
+    std::cout << "Call using pointer to the FooAdvanced object: " << f(2, "Number-2");
+
+    FN f1 = std::bind(&FooAdvanced::MakeStr, std::ref(*fObj), _1, _2);
+    std::cout << "Call using FooAdvanced reference object: " << f1(2, "Number-2");
+
+    FN f2 = std::bind(&FooAdvanced::MakeStr, *fObj, _1, _2);
+    std::cout << "Call using FooAdvanced copy object: " << f2(2, "Number-2");
+
+    delete fObj;
+    std::cout << "Call using pointer to the FooAdvanced object after delete: " << f(2, "Number-2");
+    std::cout << "Call using FooAdvanced reference object after delete: " << f1(2, "Number-2");
+    std::cout << "Call using FooAdvanced copy object after delete: " << f2(2, "Number-2");
+
+#if 0
+    FN f1 = [&](const int i, const std::string s)->std::string {
+      return fObj.MakeStr(i,s);
+    };
+    std::cout << f1(2, "Number-2");
+#endif
+  }
 }
 
 int main()
@@ -66,6 +114,7 @@ int main()
   T1::LooseFunctions();
   T1::LambdaTests();
   T1::MemberFns();
+  T1::DeletedObjects();
   return 0;
 }
 
