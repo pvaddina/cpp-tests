17476463acfb161db9af3f894726b1f98f4154ea
Prakash Rao Vaddina <prakash.vaddina@gmail.com>
2022-05-10 18:05:24 +0200

    Incomplete deletion example for vector

--- containers/vector/vec.cpp
+++ containers/vector/vec.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <ostream>
+#include <algorithm>
 
 template <typename T>
 void Print(const std::string& s, const std::vector<T>& vToPrint)
@@ -39,6 +40,10 @@ struct TDefConstructor
         std::cout << "Copy assignment\n";
         return *this;
     }
+    bool operator==(const TDefConstructor& cmpItem) const
+    {
+        return name==cmpItem.name && id==cmpItem.id;
+    }
     friend std::ostream& operator<<(std::ostream& inStr, const TDefConstructor& c)
     {
         inStr << c.name << "," << c.id;
@@ -51,6 +56,7 @@ struct TDefConstructor
 
 void InitTests()
 {
+    std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Initialization tests\n";
     // Initialization-1
     std::vector<int> one(3);  // Create a vector with 3 items initially
     Print("Size initialization of vector<int> with 3 items", one);
@@ -64,6 +70,15 @@ void InitTests()
 
     std::vector<TDefConstructor> three(3);  // Create a vector with 3 items initially
     Print("Size initialization of vector<TDefConstructor> with 3 items", three);
+
+    std::vector<TDefConstructor> four {{"Africa",1}, {"Asia",2}, {"Europe",3}, {"NorthAmerica",4}};
+    Print("vector<TDefConstructor> initialization with default objects", four);
+}
+
+void InsertionTests()
+{
+    std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Insertion tests\n";
+    std::vector<TDefConstructor> three(3);  // Create a vector with 3 items initially
 
     TDefConstructor rep {"ICE", 626};
     auto it = three.begin();
@@ -82,9 +97,113 @@ void InitTests()
     Print("Initialize second object of the vector", three);
 }
 
+void FindTests()
+{
+    std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Find tests\n";
+
+    std::vector<TDefConstructor> two{{"Africa",8}, {"Asia",22}, {"Europe",3}, {"NorthAmerica",4}};
+    Print("Contents of vector two", two);
+
+    auto findItem = TDefConstructor{"Europe",3};
+    auto it3 = std::find(two.begin(), two.end(), findItem);
+    std::cout << "Item found with std::find " << *it3 << std::endl;
+
+    auto it2 = std::find_if(two.begin(), two.end(), [&findItem](const auto& item) { return item==findItem; });
+    std::cout << "Item found with std::find_if " << *it2 << std::endl;
+}
+
+void RemovalTests()
+{
+    std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Removal tests\n";
+
+    std::vector<TDefConstructor> two{{"Africa",8}, {"Asia",22}, {"Europe",3}, {"NorthAmerica",4}};
+    Print("Contents of vector two before removing anything", two);
+
+    two.erase(two.begin());
+    Print("Contents of vector two after removing first element", two);
+
+    two.erase(two.end());
+    Print("Contents of vector two after removing end() element", two);
+}
+
+void InsertionDuringIteration()
+{
+    std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Addtion while iteration tests\n";
+
+    std::vector<TDefConstructor> continents{{"Africa",8}, {"Asia",22}, {"Europe",3}, {"NorthAmerica",4}};
+    auto initContinents = continents;
+    Print("Contents of vector continents before adding anything", continents);
+
+    auto insertItems = std::vector<std::pair<TDefConstructor,TDefConstructor>>{{{"Asia",22}        ,{"SouthAmerica",543}}
+                                                                             , {{"Africa",8}       ,{"Antarctica",3242}}
+                                                                             , {{"NorthAmerica",4} ,{"Australia", 392}} };
+
+    // Test 1: Insert items during iteration before the searched items
+    for (const auto& it2insert: insertItems)
+    {
+        for (auto it=continents.begin(); it!=continents.end(); ++it)
+        {
+            if (*it == it2insert.first)
+            {
+                // Update the iterator after the insertion
+                it = continents.insert(it, it2insert.second);
+                // Advance it by one item inorder not to keep always comparing the same item
+                std::advance(it,1);
+            }
+        }
+    }
+    Print("Test1: Inserting new elements before searched items", continents);
+
+    // Test 2: Insert items during iteration after the searched items
+    continents = initContinents;
+    for (const auto& it2insert: insertItems)
+    {
+        for (auto it=continents.begin(); it!=continents.end(); ++it)
+        {
+            if (*it == it2insert.first)
+            {
+                // Update the iterator after the insertion
+                it = continents.insert(it+1, it2insert.second);
+            }
+        }
+    }
+    Print("Test2: Inserting new elements after searched items", continents);
+}
+
+void DeletionDuringIteration()
+{
+    std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Deletion of items while iteration tests\n";
+
+    std::vector<TDefConstructor> continents{{"Africa",8}, {"SouthAmerica",543}, {"Asia",22}, {"Antarctica",3242}, {"Australia", 392}, {"Europe",3}, {"NorthAmerica",4}};
+    auto initContinents = continents;
+    Print("Contents of vector continents before adding anything", continents);
+
+    auto items2delete = std::vector<TDefConstructor>{{"Asia",22}, {"Africa",8}, {"NorthAmerica",4}};
+
+    // Test 1: Insert items during iteration before the searched items
+    for (const auto& delItem: items2delete)
+    {
+        for (auto it=continents.begin(); it!=continents.end(); ++it)
+        {
+            if (*it == delItem)
+            {
+                // Update the iterator after the insertion
+                it = continents.erase(it);
+            }
+        }
+    }
+    Print("Test1: After deletion operation", continents);
+}
+
 
 int main()
 {
     InitTests();
+    InsertionTests();
+    FindTests();
+    RemovalTests();
+    InsertionDuringIteration();
+    DeletionDuringIteration();
     return 0;
 }
+
